2b0b2c71a79a4017bcd8ef4112dad10341548de0
Garrett Steele <gsteelex@users.noreply.github.com>
2018-03-20 12:57:12 -0400

    Create basic app with "/healthCheck"

---
+++ src/java/databases/controllers/HealthCheckController.java
@@ -0,0 +1,19 @@
+package databases.controllers;
+
+import databases.HealthCheck;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Basic controller for a health-check endpoint
+ */
+@RestController
+public class HealthCheckController {
+
+    @RequestMapping(value="/healthCheck", method= RequestMethod.GET)
+    public HealthCheck healthCheck() {
+        return new HealthCheck(true);
+    }
+
+}
